456a6f9347beb93142a21ad1716e1afc5c098151
Peng Yang Guo <pyangguo@gmail.com>
2022-04-19 14:58:12 -0700

    ci: implement commitlint

--- components/layouts/primary/PrimaryLayout.tsx
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -1,17 +1,19 @@
 import Head from 'next/head';
 import styles from './PrimaryLayout.module.css';
 
-export interface IPrimaryLayout {}
+export interface IPrimaryLayout {
+	children: React.ReactNode;
+}
 
 const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
-  return (
-    <>
-      <Head>
-        <title>Primary Layout Example</title>
-      </Head>
-      <main className={styles.main}>{children}</main>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Primary Layout Example</title>
+			</Head>
+			<main className={styles.main}>{children}</main>
+		</>
+	);
 };
 
 export default PrimaryLayout;
