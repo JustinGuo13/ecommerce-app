48c8132bc4d934fdbe8915cba724449a6dc2c1f5
Peng Yang Guo <pyangguo@gmail.com>
2022-04-28 14:24:03 -0700

    ci: add storybook addon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
+		screens: {
+			xs: '375px',
+			sm: '600px',
+			md: '900px',
+			lg: '1200px',
+			xl: '1536px',
+		},
 		extend: {
 			fontFamily: {
 				lato: ["'Lato', 'sans-serif'"],
